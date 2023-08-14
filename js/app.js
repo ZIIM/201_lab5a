fffbfb07c41f16db81a39b69fd628cc5812701fc
ZIIM <johntgause12@gmail.com>
2023-08-13 21:22:12 -0700

    problem 3

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-return a + b;
+  return a + b;
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+(testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -27,7 +27,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-    return a * b;
+  return a * b;
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -47,12 +47,15 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { 
+    const sumResult = sum(sum(a,b),c);
+    const multiplyResult = multiply(multiply(a,b),c);
+    const sumString = `${a} and ${b} and ${c} sum to ${sumResult}.`; //used chatgpt
+    const multiplyString = `The product of ${a} and ${b} and ${c} is ${multiplyResult}.`; // used chatgpt
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
